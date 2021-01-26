7bcb3a74beea5b341e96031c827d246d59f1eaba
DESKTOP-CS0DS30\지영주 <wldudwn99@gmail.com>
2021-01-26 22:27:15 +0900

    스크립트

---
+++ script.js
@@ -0,0 +1,22 @@
+var button = document.getElementById('button');
+var input = document.getElementById('input');
+var list = document.getElementById('list');
+var cnt = 1;
+
+button.addEventListener('click', clickButton);
+
+function clickButton() {
+  var temp = document.createElement('li');
+  temp.setAttribute("class", "list-group-item");
+  temp.setAttribute("id", "li"+cnt);
+  temp.innerHTML = input.value;
+  temp.innerHTML += "<button style='float: right;' class='btn btn-outline-secondary' type='button' onclick='remove("+cnt+")'>삭제</button>";
+  list.appendChild(temp);
+  cnt++;
+}
+
+function remove(cnt) {
+  //window.alert(cnt);
+  var li = document.getElementById('li'+cnt);
+  list.removeChild(li);
+}
