70c1d45834de28b70d97f2f0fef7a1e3d22524d9
DESKTOP-CS0DS30\지영주 <wldudwn99@gmail.com>
2021-01-27 22:27:10 +0900

    1차 작업 (과목 입력, 수정, 삭제 가능) (디자인 x)

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ var button = document.getElementById('button');
 var input = document.getElementById('input');
 var list = document.getElementById('list');
 var cnt = 1;
-
 button.addEventListener('click', clickButton);
 
 function clickButton() {
@@ -11,10 +10,18 @@ function clickButton() {
   temp.setAttribute("id", "li"+cnt);
   temp.innerHTML = input.value;
   temp.innerHTML += "<button style='float: right;' class='btn btn-outline-secondary' type='button' onclick='remove("+cnt+")'>삭제</button>";
+  temp.innerHTML += "<button style='float: right;' class='btn btn-outline-secondary' type='button' onclick='rename("+cnt+")'>수정</button>";
   list.appendChild(temp);
   cnt++;
 }
-
+function rename(cnt) {
+  //window.alert(cnt);
+  var li = document.getElementById('li'+cnt);
+  var newText = prompt("무엇으로 수정하시겠습니까?")
+  li.innerHTML = newText;
+  li.innerHTML += "<button style='float: right;' class='btn btn-outline-secondary' type='button' onclick='remove("+cnt+")'>삭제</button>";
+  li.innerHTML += "<button style='float: right;' class='btn btn-outline-secondary' type='button' onclick='rename("+cnt+")'>수정</button>";
+}
 function remove(cnt) {
   //window.alert(cnt);
   var li = document.getElementById('li'+cnt);
