006a448c31f31c9d5d813065c471bfc861ba27b3
mooyoun2309 <mooyoung2309@gmail.com>
2021-01-28 22:15:59 +0900

    타이머 기능 추가

--- script_cp.js
+++ script_cp.js
@@ -1,12 +1,42 @@
 var button = document.getElementsByClassName('goal_btn')[0];
-var text = document.getElementsByClassName('goal_txt');
-var temp_btn = document.getElementById('button');
 
 button.addEventListener('click', clickButton);
-temp_btn.addEventListener('click', clickButton);
+
+var bool_onclick = false
+var color = "#B291EB";
+var stTime;
+var endTime = 0;
+
+var min;
+var sec;
+var milisec;
+var nowTime;
+var militime = 0;
+var time = 0;
 function clickButton() {
-  var color = ["#FC5C&D", "#6A82FB", "38ef7d"]
-  var num = Math.floor(Math.random() * color.length);
-  button.backgroundColor = color[num];
-  alert("등록되었습니다");
+
+  if (bool_onclick === false) {
+    button.style.background = color;
+    bool_onclick = true;
+
+    timerStart = setInterval(function() {
+      time++;
+      hour = addZero(parseInt(time/3600));
+      min = addZero(parseInt((time%3600)/60));
+      sec = addZero(parseInt(time%60));
+      document.getElementById('goal_txt').innerHTML = hour+":"+min+":"+sec;
+      button.innerHTML = "눌러보세요!"
+      console.log(milisec)
+    },1000);
+  } else {
+    if(timerStart) {
+      clearInterval(timerStart);
+      button.style.background = "#FFFFFF";
+      bool_onclick = false;
+    }
+  }
+}
+
+function addZero(num) {
+  return (num < 10 ? '0'+num : ''+num)
 }
