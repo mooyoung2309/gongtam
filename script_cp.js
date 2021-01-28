d9716a5224ce6aef52172701a72e474b1281b51b
mooyoun2309 <mooyoung2309@gmail.com>
2021-01-28 20:11:42 +0900

    checkingpage

--- script_cp.js
+++ script_cp.js
@@ -1,6 +1,6 @@
-var button = document.getElementsByClassName('goal_btn');
-var text = document.getElementsByClassName('goal_txt')
-var temp_btn = document.getElementById('button')
+var button = document.getElementsByClassName('goal_btn')[0];
+var text = document.getElementsByClassName('goal_txt');
+var temp_btn = document.getElementById('button');
 
 button.addEventListener('click', clickButton);
 temp_btn.addEventListener('click', clickButton);
@@ -8,5 +8,5 @@ function clickButton() {
   var color = ["#FC5C&D", "#6A82FB", "38ef7d"]
   var num = Math.floor(Math.random() * color.length);
   button.backgroundColor = color[num];
-  alert("등록되었습니다")
+  alert("등록되었습니다");
 }
