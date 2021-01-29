57b5b3d73f7a33bbc7b0c32f9c1feed4f72b7d9d
DESKTOP-CS0DS30\지영주 <wldudwn99@gmail.com>
2021-01-29 23:32:55 +0900

    no message

--- script.js
+++ script.js
@@ -8,20 +8,22 @@ clear_button.addEventListener('click', clear);
 function clickButton() {
   var temp = document.createElement('li');
   temp.setAttribute("class", "list-group-item");
-  temp.setAttribute("id", "li"+cnt);
   temp.innerHTML = input.value;
   temp.innerHTML += "<button style='float: right;' type='button' onclick='remove("+cnt+")'>삭제</button>";
-  temp.innerHTML += "<button style='float: right;' type='button' onclick='rename("+cnt+")'>수정</button>";
+  temp.innerHTML += "<button style='float: right; background-color:#369F36' type='button' onclick='rename("+cnt+")'>수정</button>";
   list.appendChild(temp);
   cnt++;
+}
+function doubleClick(){
+  rename(cnt)
 }
 function rename(cnt) {
   //window.alert(cnt);
   var li = document.getElementById('li'+cnt);
   var newText = prompt("무엇으로 수정하시겠습니까?")
   li.innerHTML = newText;
-  li.innerHTML += "<button style='float: right;' class='btn btn-outline-secondary' type='button' onclick='remove("+cnt+")'>삭제</button>";
-  li.innerHTML += "<button style='float: right;' class='btn btn-outline-secondary' type='button' onclick='rename("+cnt+")'>수정</button>";
+  li.innerHTML += "<button style='float: right;' type='button' onclick='remove("+cnt+")'>삭제</button>";
+  li.innerHTML += "<button style='float: right; background-color:#369F36;' color ='#369F36' type='button' onclick='rename("+cnt+")'>수정</button>";
 }
 function remove(cnt) {
   //window.alert(cnt);
